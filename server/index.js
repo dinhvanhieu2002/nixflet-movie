88eb9f9cdd1580a8823c5b656abb106588346bf1
hieudinh2002 <dinhvanhieu2002@gmail.com>
2025-01-13 22:26:17 +0700

    fix mongoose warning

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import http from "http";
-import mongoose from "mongoose";
-import "dotenv/config";
-import routes from "./src/routes/index.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import http from 'http';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import routes from './src/routes/index.js';
 
 const app = express();
 
@@ -13,21 +13,22 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1", routes);
+app.use('/api/v1', routes);
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
+mongoose.set('strictQuery', true);
 mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("mongodb connected");
-    server.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log({ err });
-    process.exit(1);
-  });
+	.connect(process.env.MONGODB_URL)
+	.then(() => {
+		console.log('mongodb connected');
+		server.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
+	})
+	.catch((err) => {
+		console.log({ err });
+		process.exit(1);
+	});
